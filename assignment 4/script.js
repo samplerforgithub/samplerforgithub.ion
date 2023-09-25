4eb1ca2e68967f40d7b486ae10cad383719cfebe
samplerforgithub <singhhimanshi1447@gmail.com>
2023-09-25 23:15:18 +0530

    update

---
+++ assignment 4/script.js
@@ -0,0 +1,18 @@
+var names=new Array();
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="Jason";
+names[4]="paul";
+names[5]="Frank";
+names[6]="larry";
+names[7]="Paula";
+names[8]="laura";
+names[9]="jim";
+
+for (var i=0; i<names.length; i++){
+	if(n[i].chartAt()=='j'||n[i]==='j')
+	{
+		console.log("Goodbye"+names[i])
+	}
+}
